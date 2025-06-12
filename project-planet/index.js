b2c588f8528b56be086f122caaf92243c9a52816
Ezeh20 <ranter1570@gmail.com>
2025-06-12 21:23:32 +0100

    chore: read and parse the csv file then get the amount of habitable planets

--- project-planet/index.js
+++ project-planet/index.js
@@ -1,2 +1,32 @@
-const {parse} = require("csv-parse")
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const result = [];
 
+const isLiveable = (planet) => {
+	return (
+		planet["koi_disposition"] === "CONFIRMED" &&
+		planet["koi_insol"] > 0.36 &&
+		planet["koi_insol"] < 1.11 &&
+		planet["koi_prad"] < 1.61
+	);
+};
+
+fs.createReadStream("exoplanet-nasa.csv")
+	.pipe(
+		parse({
+			comment: "#",
+			columns: true,
+		})
+	)
+	.on("data", (data) => {
+		if (isLiveable(data)) {
+			result.push(data);
+		}
+	})
+	.on("error", (error) => {
+		console.log(error);
+	})
+	.on("end", () => {
+		console.log(`${result.length} planets found`);
+		console.log("End of stream");
+	});
